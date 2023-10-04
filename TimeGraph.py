19e53f54288b2a489465d92bf81d4eae31ee00c5
Muyeed Ahmed <muyeedahmed@muyeeds-air.mynetworksettings.com>
2023-10-04 17:45:33 -0400

    TimeGraph.py edit to include SkIF n_e

--- TimeGraph.py
+++ TimeGraph.py
@@ -4,6 +4,8 @@ import pandas as pd
 import mat73
 from scipy.io import loadmat
 from sklearn.covariance import EllipticEnvelope
+from sklearn.ensemble import IsolationForest
+
 from sklearn.metrics.cluster import adjusted_rand_score
 
 import numpy as np
@@ -20,6 +22,9 @@ import warnings
 warnings.filterwarnings(action='ignore')
 
 datasetFolderDir = 'Dataset/'
+
+implementation = "SkIF"
+parameter_st = "n_estimators"
 
 def ReadFile(filename):
     print(filename)
@@ -57,6 +62,49 @@ def ReadFile(filename):
     return X, gt
     
     
+
+def runIF(filename, X, gt, runs):
+    times = []
+    flips = []
+    aris = []
+    n_es = [50, 100, 150, 200, 250, 300, 350, 400, 450, 500]
+    for n_e in n_es:
+        print(n_e, end=",")
+        labels = []
+        timeElapsed = []
+        ari = []
+        m_ari =[]
+        for i in range(runs):
+            t1_start = process_time()
+            
+            clustering = IsolationForest(n_estimators=n_e).fit(X)
+
+            t1_stop = process_time()
+            timeElapsed.append(t1_stop-t1_start)
+    
+            l = clustering.predict(X)
+            l = [0 if x == 1 else 1 for x in l]
+            ari.append(adjusted_rand_score(gt, l))
+            labels.append(l)
+        
+        avgTimeElapsed = sum(timeElapsed)/len(timeElapsed)
+        aris.append(np.mean(ari))
+        flipped = flip_count(filename, gt, labels, runs)
+        times.append(avgTimeElapsed)
+        flips.append(flipped)
+    print()
+
+    draw(filename, n_es,flips,times, aris)
+    
+    t = times/(np.max(times))
+    f = flips/(np.max(flips))
+    a = [(x-np.min(aris))/(np.max(aris) - np.min(aris)) for x in aris]
+    
+    time_slope, _, _, _, _ = stats.linregress(n_es, t)
+    flip_slope, _, _, _, _ = stats.linregress(n_es, f)
+    ari_slope, _, _, _, _ = stats.linregress(n_es, a)
+    
+    return time_slope, flip_slope, ari_slope
     
     
 def runEE(filename, X, gt, runs):
@@ -86,13 +134,9 @@ def runEE(filename, X, gt, runs):
             l = [0 if x == 1 else 1 for x in l]
             ari.append(adjusted_rand_score(gt, l))
             labels.append(l)
-        # for i in range(len(labels)):
-        #     for j in range(i+1, len(labels)):            
-        #         m_ari.append(adjusted_rand_score(labels[i], labels[j]))
         
         avgTimeElapsed = sum(timeElapsed)/len(timeElapsed)
         aris.append(np.mean(ari))
-        # m_aris.append(np.mean(m_ari))
         flipped = flip_count(filename, gt, labels, runs)
         times.append(avgTimeElapsed)
         flips.append(flipped)
@@ -102,7 +146,6 @@ def runEE(filename, X, gt, runs):
     # print(aris)
 
     draw(filename, sfs,flips,times, aris)
-    # print(t)
     t = times/(np.max(times))
     f = flips/(np.max(flips))
     a = [(x-np.min(aris))/(np.max(aris) - np.min(aris)) for x in aris]
@@ -135,7 +178,7 @@ def draw(filename, sfs, f, t, a):
     fig,ax = plt.subplots()
     
     plt1 = ax.plot(sfs, f, color="red", marker="o", label="Flips")
-    plt2 = ax.plot(sfs, a, color="orange", marker='o', label="ARI")
+    # plt2 = ax.plot(sfs, a, color="orange", marker='o', label="ARI")
     # plt3 = ax.plot(sfs, ma, color="green", marker='o', label="Mutual ARI")
     
     ax.set_xlabel("support_fraction", fontsize = 12)
@@ -149,7 +192,7 @@ def draw(filename, sfs, f, t, a):
     ax.legend()
     plt.show()
     
-    fig.savefig('Fig/Time/SkEE_'+filename+'.pdf', bbox_inches='tight')
+    fig.savefig('Fig/Time/'+implementation+'_'+parameter_st+'_'+filename+'.pdf', bbox_inches='tight')
 
 # sfs = [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]
 # tt = [0.7555669999999985, 0.7542099, 0.7787064000000001, 0.8093423999999985, 0.7836930000000024, 0.7779766999999964, 0.7921503000000001, 0.8128416999999999, 0.8052817000000004]
@@ -190,17 +233,17 @@ def drawBeanPlot(bt, bf, ba):
     sns.violinplot(data=df, x='Group', y='Value')
     plt.xlabel('')
     plt.ylabel('')
-    plt.savefig('Fig/Slope_SkEE_SF.pdf', bbox_inches='tight')
+    plt.savefig("Fig/Slope_"+implementation+"_"+parameter_st+".pdf", bbox_inches='tight')
 
     plt.show()
 
 def save_slope(filename, t_b, f_b, a_b):
-    if os.path.exists("Stats/Slope_suport_fraction.csv") == 0:
-        f=open("Stats/Slope_suport_fraction.csv", "w")
+    if os.path.exists("Stats/Slope_"+implementation+"_"+parameter_st+".csv") == 0:
+        f=open("Stats/Slope_"+implementation+"_"+parameter_st+".csv", "w")
         f.write('Filename,Time_slope,Flip_slope,ARI_Slope\n')
         f.close()
 
-    f=open("Stats/Slope_suport_fraction.csv", "a")
+    f=open("Stats/Slope_"+implementation+"_"+parameter_st+".csv", "a")
     f.write(filename+','+str(t_b)+','+str(f_b)+','+str(a_b)+'\n')
     f.close()
     
@@ -213,9 +256,9 @@ if __name__ == '__main__':
     for i in range(len(master_files)):
         master_files[i] = master_files[i].split("/")[-1].split(".")[0]
     
-    beta_ts = []
-    beta_fs = []
-    beta_as = []
+    slope_ts = []
+    slope_fs = []
+    slope_as = []
     
     runs = 10
     
@@ -224,12 +267,13 @@ if __name__ == '__main__':
         #     # print(fname, " already done!")
         #     continue
         X, gt = ReadFile(fname)
-        t_b, f_b, a_b = runEE(fname, X, gt, runs)
-        print("Betas: ", t_b, f_b, a_b)
+        # t_b, f_b, a_b = runEE(fname, X, gt, runs)
+        t_b, f_b, a_b = runIF(fname, X, gt, runs)
+        print("Slopes: ", t_b, f_b, a_b)
         save_slope(fname, t_b, f_b, a_b)
-        beta_ts.append(t_b)
-        beta_fs.append(f_b)
-        beta_as.append(a_b)
+        slope_ts.append(t_b)
+        slope_fs.append(f_b)
+        slope_as.append(a_b)
     # print("b_t = ", beta_ts)
     # print("b_f = ", beta_fs)
     # print("b_a = ", beta_as)
@@ -237,5 +281,5 @@ if __name__ == '__main__':
     # beta_fs =  [0.632811055602595, 0.5027437597858563, 1.373033983901582, 0.3068056145659396, 0.2018284242394247, 0.13233359828767663, 0.7305266795304689, 0.2507232637503445, 0.6067746578695484, 0.9941002949852511, 0.12369315462301433, 0.8668260267735418, 1.1158816016828184, 0.6248606179129569, 0.24921724517829397, 0.755777298760859, 1.4108655019426313, 1.4365493326346777, 1.3417349275716157, 0.48430059953720883, 0.7129173954941178, 1.499707651341638, 1.0168601925520275, -0.0665015960983665, 1.0396618191481777, 0.7939078198215282, 1.238260278627251, 1.0363893551421233, 0.34842708362335745, 0.31317532624722183, 0.11574347984240319, -0.034003968123053036, 0.5344297704403832, 1.0608897105445454, 0.6676629978976147, 1.243208859235073, 0.9486780723483945, 1.926507682837043, 0.5185232613145191, 1.1622532498796345, 0.9883774157094425, 0.6412614009822386, 1.3014338362848479, 1.4212127644020975, np.nan, 0.9019999999999999, -0.09876543209876547, 0.32579035827107294, -0.09876543209876547, 0.2008568946252856, np.nan, 0.9650981790548051, 0.3643133272762904, 1.1856056864945748, 1.0867373698837546]
     # beta_as =  [0.4840786478821617, 0.3069455035932473, 1.0385304797352426, 0.5848138054492341, -0.0026579702564136277, 0.1780133602285725, 0.2460836950976305, 0.293259050875669, 0.1522723085909422, 0.5659230582206488, 0.34216114390518365, 1.0760903254763359, 0.16798116544985608, 0.1964641161315091, np.nan, -0.09876543209876547, 1.4295191412569581, 0.7482243878003476, 0.13301492368736761, 0.7025904275182135, 1.108942928021392, 0.6354061932837098, 0.731993179861633, np.nan, 0.5119566086023036, 0.38847483791305937, 0.8743144185884312, 0.378456540370215, 0.8507031376203663, 0.053099180466042076, 0.19923262368917022, -0.07407407407407404, 0.9062448567937937, 0.6491681279517363, 0.2862575651123984, 0.30375307932325973, 1.1657594028428275, 0.9128229612198151, 0.37156275037610614, 0.5715533454026411, 0.20929035578412464, 0.23613212034105271, 0.880597842813497, 0.6746577184499133, np.nan, 0.6496046348246571, -0.012345679012345652, 0.23435518357628, -0.09876543209876547, 0.06442442514793251, np.nan, 1.24547268246242, 0.48610531987811884, 0.4465658006229036, 0.611508970326768]
 
-    drawBeanPlot(beta_ts, beta_fs, beta_as)
+    drawBeanPlot(slope_ts, slope_fs, slope_as)
     
