b28937f3d7ece8967845c02a4b3d9023c12e810c
Muyeed Ahmed <muyeedahmed@muyeeds-air.mynetworksettings.com>
2023-10-06 17:13:15 -0400

    ND Source example changed

--- NDSourceExample.py
+++ NDSourceExample.py
@@ -10,8 +10,8 @@ np.random.seed(3)
 all_indices = list(range(12))
 np.random.shuffle(all_indices)
 
-x_indices = all_indices[:2]
-o_indices = all_indices[4:6]
+x_indices = all_indices[:5]
+o_indices = all_indices[4:9]
 
 
 # Mark 4 points as 'x'
@@ -30,8 +30,8 @@ plt.rcParams['figure.figsize'] = [6, 4]
 
 # Plot the points
 plt.scatter(x_coordinates, y_coordinates, marker='.', label='Unpicked', color="black", s=100)
-plt.scatter(x_x, x_y, marker='^', label='Estimator_1', color="orange", s=200)
-plt.scatter(o_x, o_y, marker='v', label='Estimator_2', color="blue", s=200)
+plt.scatter(x_x, x_y, marker='v', label='Run 1', color="orange", s=150)
+plt.scatter(o_x, o_y, marker='^', label='Run 2', color="blue", s=150)
 
 # Add labels and legend
 plt.xlabel('')
@@ -43,7 +43,7 @@ plt.legend()
 
 # Show the plot
 plt.grid()
-plt.savefig("Fig/NDSource/Resource_Run1.pdf", bbox_inches='tight')
+plt.savefig("Fig/NDSource/Restart.pdf", bbox_inches='tight')
 
 plt.show()
 
